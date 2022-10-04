0d24898926f46e6eb7ad461b968f51a385b14f17
Ruben Olano <rubendavid2711@gmail.com>
2022-10-04 13:28:40 -0500

    Remove refetch on window focus to reduce loading constantly

--- src/components/AdminUserComp.tsx
+++ src/components/AdminUserComp.tsx
@@ -1,53 +1,69 @@
 import { GroupMember } from "@prisma/client";
 import Image from "next/image";
 import React, { FC } from "react";
+import { useQueryClient } from "react-query";
 import { getImage } from "../utils/defaultUserImage";
 import { trpc } from "../utils/trpc";
 
 interface Props {
-  member: GroupMember;
+	member: GroupMember;
 }
 
 const AdminUserComp: FC<Props> = ({ member }) => {
-  const { data: user } = trpc.useQuery([
-    "users.getUser",
-    { id: member.userId },
-  ]);
+	const queryClient = useQueryClient();
+	const { data: user } = trpc.useQuery([
+		"users.getUser",
+		{ id: member.userId },
+	]);
 
-  if (!user) {
-    return null;
-  }
+	const { mutate } = trpc.useMutation("groups.removeGroupMember", {
+		onSuccess: () => {
+			queryClient.invalidateQueries("groups.fetchGroupNonAdmins");
+			queryClient.refetchQueries("groups.fetchGroupNonAdmins");
+		},
+	});
 
-  return (
-    <div className="flex flex-row items-center justify-center m-2">
-      <Image
-        src={getImage(user.image)}
-        alt={`${user.fname} ${user.lname}`}
-        width={30}
-        height={30}
-        className="rounded-full"
-      />
-      <h1 className="text-cente ml-2">
-        {user.fname} {user.lname}
-      </h1>
-      <button className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-    </div>
-  );
+	const onclick = () => {
+		mutate({ memberId: member.id });
+	};
+
+	if (!user) {
+		return null;
+	}
+
+	return (
+		<div className="flex flex-row items-center justify-center m-2">
+			<Image
+				src={getImage(user.image)}
+				alt={`${user.fname} ${user.lname}`}
+				width={30}
+				height={30}
+				className="rounded-full"
+			/>
+			<h1 className="text-cente ml-2">
+				{user.fname} {user.lname}
+			</h1>
+			<button
+				className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+				onClick={onclick}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth={2}
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			</button>
+		</div>
+	);
 };
 
 export default AdminUserComp;
--- src/schema/group.schema.ts
+++ src/schema/group.schema.ts
@@ -1,62 +1,68 @@
 import z from "zod";
 
 export const createGroupSchema = z.object({
-  userId: z.string(),
-  name: z.string(),
-  description: z.string().optional(),
+	userId: z.string(),
+	name: z.string(),
+	description: z.string().optional(),
 });
 
 export type createGroupInput = z.TypeOf<typeof createGroupSchema>;
 
 export const createGroupOutputSchema = z.object({
-  groupId: z.string(),
-  name: z.string(),
-  description: z.string().optional(),
+	groupId: z.string(),
+	name: z.string(),
+	description: z.string().optional(),
 });
 
 export type createGroupOutput = z.TypeOf<typeof createGroupOutputSchema>;
 
 export const fetchGroupsSchema = z.object({
-  userId: z.string(),
+	userId: z.string(),
 });
 
 export type fetchGroupsInput = z.TypeOf<typeof fetchGroupsSchema>;
 
 export const fetchGroupSchema = z.object({
-  id: z.string(),
+	id: z.string(),
 });
 
 export const fetchUserIsAdminSchema = z.object({
-  userId: z.string(),
-  groupId: z.string(),
+	userId: z.string(),
+	groupId: z.string(),
 });
 
 export type fetchUserIsAdminInput = z.TypeOf<typeof fetchUserIsAdminSchema>;
 
 export const fetchGroupAdminsSchema = z.object({
-  groupId: z.string(),
+	groupId: z.string(),
 });
 
 export type fetchGroupAdminsInput = z.TypeOf<typeof fetchGroupAdminsSchema>;
 
 export const fetchGroupNonAdminsSchema = z.object({
-  groupId: z.string(),
+	groupId: z.string(),
 });
 
 export type fetchGroupNonAdminsInput = z.TypeOf<
-  typeof fetchGroupNonAdminsSchema
+	typeof fetchGroupNonAdminsSchema
 >;
 
 export const removeGroupAdminSchema = z.object({
-  adminId: z.string(),
+	adminId: z.string(),
 });
 
 export type removeGroupAdminInput = z.TypeOf<typeof removeGroupAdminSchema>;
 
 export const removeUserFromGroupSchema = z.object({
-  memberId: z.string(),
+	memberId: z.string(),
 });
 
+export type removeUserFromGroupInput = z.TypeOf<
+	typeof removeUserFromGroupSchema
+>;
+
 export const deleteGroupSchema = z.object({
-  groupId: z.string(),
+	groupId: z.string(),
 });
+
+export type deleteGroupInput = z.TypeOf<typeof deleteGroupSchema>;
