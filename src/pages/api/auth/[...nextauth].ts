fc90aa98d094ea71727e6625e5640bc1ba1b6205
Ruben Olano <rubendavid2711@gmail.com>
2022-10-04 12:40:23 -0500

    Update email signup to automatically sign in once completed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,155 +11,169 @@ import { encode, decode } from "next-auth/jwt";
 import Cookies from "cookies";
 
 export const options: NextAuthOptions = {
-  // Include user.id on session
-  providers: [],
-  adapter: PrismaAdapter(prisma),
-  secret: env.NEXTAUTH_SECRET,
+	// Include user.id on session
+	providers: [],
+	adapter: PrismaAdapter(prisma),
+	secret: env.NEXTAUTH_SECRET,
 };
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, {
-    // Include user.id on session
-    callbacks: {
-      session({ session, user }) {
-        if (session.user) {
-          session.user.id = user.id;
-          session.user.name = user.fname + " " + user.lname;
-        }
-        return session;
-      },
-      jwt({ token, user }) {
-        if (user) {
-          token.id = user.id;
-        }
-        return token;
-      },
-      async signIn({ user }) {
-        if (
-          req?.query?.nextauth?.includes("callback") &&
-          req?.query?.nextauth?.includes("credentials") &&
-          req.method === "POST"
-        ) {
-          if (user) {
-            const sessionToken = generateSessionToken();
-            const sessionExpiry = fromDate(3600);
+	return NextAuth(req, res, {
+		// Include user.id on session
+		callbacks: {
+			session({ session, user }) {
+				if (session.user) {
+					session.user.id = user.id;
+					session.user.name = user.fname + " " + user.lname;
+				}
+				return session;
+			},
+			jwt({ token, user }) {
+				if (user) {
+					token.id = user.id;
+				}
+				return token;
+			},
+			async signIn({ user }) {
+				if (
+					req?.query?.nextauth?.includes("callback") &&
+					req?.query?.nextauth?.includes("credentials") &&
+					req.method === "POST"
+				) {
+					if (user) {
+						const sessionToken = generateSessionToken();
+						const sessionExpiry = fromDate(3600);
 
-            await prisma.user.update({
-              where: {
-                id: user.id,
-              },
-              data: {
-                sessions: {
-                  create: {
-                    sessionToken,
-                    expires: sessionExpiry,
-                  },
-                },
-              },
-            });
+						await prisma.user.update({
+							where: {
+								id: user.id,
+							},
+							data: {
+								sessions: {
+									create: {
+										sessionToken,
+										expires: sessionExpiry,
+									},
+								},
+							},
+						});
 
-            const cookies = new Cookies(req, res);
+						const cookies = new Cookies(req, res);
 
-            cookies.set("next-auth.session-token", sessionToken, {
-              expires: sessionExpiry,
-            });
-          }
-        }
+						cookies.set("next-auth.session-token", sessionToken, {
+							expires: sessionExpiry,
+						});
+					}
+				}
 
-        return true;
-      },
-    },
-    pages: {
-      signIn: "/auth/signin",
-    },
-    adapter: PrismaAdapter(prisma),
-    jwt: {
-      secret: env.JWT_SECRET,
-      async encode({ token, secret, maxAge }) {
-        if (
-          req?.query?.nextauth?.includes("callback") &&
-          req?.query?.nextauth?.includes("credentials") &&
-          req.method === "POST"
-        ) {
-          const cookies = new Cookies(req, res);
+				return true;
+			},
+		},
+		pages: {
+			signIn: "/auth/signin",
+		},
+		adapter: PrismaAdapter(prisma),
+		jwt: {
+			secret: env.JWT_SECRET,
+			async encode({ token, secret, maxAge }) {
+				if (
+					req?.query?.nextauth?.includes("callback") &&
+					req?.query?.nextauth?.includes("credentials") &&
+					req.method === "POST"
+				) {
+					const cookies = new Cookies(req, res);
 
-          const cookie = cookies.get("next-auth.session-token");
+					const cookie = cookies.get("next-auth.session-token");
 
-          if (cookie) return cookie;
-          else return "";
-        }
-        return encode({ token, secret, maxAge });
-      },
-      async decode({ token, secret }) {
-        if (
-          req?.query?.nextauth?.includes("callback") &&
-          req.query.nextauth.includes("credentials") &&
-          req.method === "POST"
-        ) {
-          return null;
-        }
+					if (cookie) return cookie;
+					else return "";
+				}
+				return encode({ token, secret, maxAge });
+			},
+			async decode({ token, secret }) {
+				if (
+					req?.query?.nextauth?.includes("callback") &&
+					req.query.nextauth.includes("credentials") &&
+					req.method === "POST"
+				) {
+					return null;
+				}
 
-        return decode({ token, secret });
-      },
-    },
-    providers: [
-      GoogleProvider({
-        clientId: env.GOOGLE_CLIENT_ID,
-        clientSecret: env.GOOGLE_CLIENT_SECRET,
-        authorization: {
-          params: {
-            access_type: "offline",
-            prompt: "consent",
-            response_type: "code",
-          },
-        },
-        async profile(profile) {
-          return {
-            id: profile.sub,
-            fname: profile.given_name,
-            lname: profile.family_name,
-            email: profile.email,
-            password: profile.sub,
-          };
-        },
-      }),
-      CredentialsProvider({
-        name: "Email",
-        credentials: {
-          email: {
-            label: "Email",
-            type: "text",
-            placeholder: "rolano@example.com",
-          },
-          password: {
-            label: "Password",
-            type: "password",
-          },
-        },
-        authorize: async (credentials) => {
-          if (!credentials || !credentials.email || !credentials.password) {
-            return null;
-          }
-          const user = await prisma.user.findUnique({
-            where: {
-              email: credentials.email,
-            },
-          });
-          if (!user) {
-            return null;
-          }
-          const isMatch = await compare(credentials.password, user.password);
+				return decode({ token, secret });
+			},
+		},
+		providers: [
+			GoogleProvider({
+				clientId: env.GOOGLE_CLIENT_ID,
+				clientSecret: env.GOOGLE_CLIENT_SECRET,
+				authorization: {
+					params: {
+						access_type: "offline",
+						prompt: "consent",
+						response_type: "code",
+					},
+				},
+				async profile(profile) {
+					return {
+						id: profile.sub,
+						fname: profile.given_name,
+						lname: profile.family_name,
+						email: profile.email,
+						password: profile.sub,
+					};
+				},
+			}),
+			CredentialsProvider({
+				name: "Email",
+				credentials: {
+					email: {
+						label: "Email",
+						type: "text",
+						placeholder: "rolano@example.com",
+					},
+					password: {
+						label: "Password",
+						type: "password",
+					},
+				},
+				authorize: async (credentials) => {
+					if (
+						!credentials ||
+						!credentials.email ||
+						!credentials.password
+					) {
+						return null;
+					}
+					const user = await prisma.user.findUnique({
+						where: {
+							email: credentials.email,
+						},
+					});
+					if (!user) {
+						// Create new user and confirm if passwords match
+						const newUser = await prisma.user.create({
+							data: {
+								email: credentials.email,
+								password: credentials.password,
+							},
+						});
+						return newUser;
+					}
+					const isMatch = await compare(
+						credentials.password,
+						user.password
+					);
 
-          if (!isMatch) {
-            return null;
-          }
-          return user;
-        },
-      }),
+					if (!isMatch) {
+						return null;
+					}
+					return user;
+				},
+			}),
 
-      // ...add more providers here
-    ],
-    secret: env.NEXTAUTH_SECRET,
-    debug: true,
-  });
+			// ...add more providers here
+		],
+		secret: env.NEXTAUTH_SECRET,
+		debug: true,
+	});
 }
