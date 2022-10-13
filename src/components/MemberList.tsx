f98b92f201723532b764873a977129efb8ccbf29
Ruben Olano <rubendavid2711@gmail.com>
2022-10-13 10:33:48 -0500

    update interfaces

--- src/components/MemberList.tsx
+++ src/components/MemberList.tsx
@@ -3,29 +3,30 @@ import { FC } from "react";
 import UserComp from "./UserComp";
 
 interface Props {
-  members: (GroupMember & {
-    User: {
-      fname: string | null;
-      lname: string | null;
-      image: string | null;
-      id: string;
-    };
-  })[];
+	members: (GroupMember & {
+		User: {
+			name: string | null;
+			image: string | null;
+			id: string;
+		};
+	})[];
 }
 
 const MemberList: FC<Props> = ({ members }) => {
-  return (
-    <>
-      <div className="backdrop-sepia-0 rounded-sm bg-white/75 p-3 justify-center">
-        <h2 className="text-center text-2xl font-bold">Group Members</h2>
-        <div className="flex flex-col flex-wrap justify-center items-center overflow-y-scroll max-h-[50vh]">
-          {members.map((member) => (
-            <UserComp key={member.id} user={member} />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="backdrop-sepia-0 rounded-sm bg-white/75 p-3 justify-center">
+				<h2 className="text-center text-2xl font-bold">
+					Group Members
+				</h2>
+				<div className="flex flex-col flex-wrap justify-center items-center overflow-y-scroll max-h-[50vh]">
+					{members.map((member) => (
+						<UserComp key={member.id} user={member} />
+					))}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default MemberList;
