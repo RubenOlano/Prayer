247e48ed33435c3eea23fee5b7d33b7e87b051d6
Ruben Olano <rubendavid2711@gmail.com>
2022-12-17 23:02:45 -0600

    Update time

--- src/pages/api/ping.tsx
+++ src/pages/api/ping.tsx
@@ -5,7 +5,9 @@ const handler: NextApiHandler = (req, res) => {
   // Check if the request has the correct headers
   const { headers } = req;
   if (headers["secret-key"] !== env.JWT_SECRET) return res.status(401).json({ error: "Invalid secret key" });
-  const time = Date.now().toLocaleString();
+  // Get the current time
+  const time = new Date().toLocaleDateString();
+
   res.status(200).json({ time });
 };
 
